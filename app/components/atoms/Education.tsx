5060a5b85c94221d4ba67c52366bd416d361c783
Mohammad Almechkor <mohamed.almechkor@bienici.com>
2024-10-18 12:21:05 +0200

    refact(Eductions): split Education into small components

---
+++ app/components/atoms/Education.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react";
+import { motion } from "framer-motion";
+import { EducationModel } from "../../models";
+
+type EducationProps = {
+  education: EducationModel;
+  index: number;
+  visible: boolean;
+};
+
+export const Education: FC<EducationProps> = ({ education, index, visible }) => {
+  return (
+    <motion.li
+      key={index}
+      className="education-item  bg-white "
+      initial={{ opacity: 0, translateY: 50 }}
+      animate={
+        visible
+          ? { opacity: 1, translateY: 0 }
+          : {}
+      }
+      transition={{ duration: 0.8, ease: "easeOut" }}
+      data-index={index}
+    >
+      <p className="font-semibold">
+        {education.degree} in {education.major ? education.major : education.degree}
+      </p>
+      <p>
+        {education.institution}, {education.duration}
+      </p>
+    </motion.li>
+  );
+};
+
+export default Education;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import About from "./components/About";
 import ContactDetails from "./components/ContactDetails";
-import Education from "./components/Education";
+import Educations from "./components/Educations";
 import FurtherEducation from "./components/FurtherEducation";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
@@ -34,7 +34,7 @@ export default function Home() {
                 <div className="w-full border-l-2 border-gray-400">
                   <About about={data.personalInfo.about} />
                   <WorkExperience experiences={data.experiences} />
-                  <Education educations={data.educations} />
+                  <Educations educations={data.educations} />
                 </div>
               </main>
             </div>
