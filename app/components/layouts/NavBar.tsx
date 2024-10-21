f05cf3dcf833cbc2f2c897cc4691b09a065e800f
adhamito <aalmechkor2@gmail.com>
2024-10-21 22:13:37 +0100

    Update NavBar layout component

--- app/components/layouts/NavBar.tsx
+++ app/components/layouts/NavBar.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { FaGithub } from "react-icons/fa";
 import { CgMail } from "react-icons/cg";
 import Link from "next/link";
@@ -13,24 +11,18 @@ export const NavBar = () => {
         <div></div>
         <div className="flex items-center gap-4">
           <Link href="https://github.com/adhamito/cv-template" target="_blank">
-            <div
-              className={"p-2 rounded-full"}
-            >
+            <div className={"p-2 rounded-full"}>
               <FaGithub size={25} className="text-white" />
             </div>
           </Link>
 
           <Link href="mailto:aalmechkor2@gmail.com" target="_blank">
-            <div
-              className={"p-2 rounded-full"}
-            >
+            <div className={"p-2 rounded-full"}>
               <CgMail size={25} className="text-white" />
             </div>
           </Link>
 
-          <div
-            className={"p-2 rounded-full"}
-          >
+          <div className={"p-2 rounded-full"}>
             <DownloadPDFButton />
           </div>
         </div>
