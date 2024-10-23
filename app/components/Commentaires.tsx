80532b60b1bff2aed0fa82439b9b71efcae3a92d
adhamito <aalmechkor2@gmail.com>
2024-10-23 01:08:36 +0100

    Add Commentaires component

---
+++ app/components/Commentaires.tsx
@@ -0,0 +1,152 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion"; // Import Framer Motion
+import { CommentModal } from "./modals/CommentModal";
+import { CommentModalTrigger } from "./modals/CommentModalTrigger";
+import { Modal } from "./ui/animated-modal";
+
+const Commentaires = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [comments, setComments] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const fetchComments = async () => {
+    try {
+      const response = await fetch("/api/comments");
+      if (response.ok) {
+        const data = await response.json();
+        setComments(data.comments || []);
+      } else {
+        const error = await response.json();
+        setErrorMessage(error.error || "Failed to fetch comments");
+      }
+    } catch (error) {
+      console.error("Error fetching comments:", error);
+      setErrorMessage("Something went wrong while fetching comments.");
+    }
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/api/comments", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          description: message,
+        }),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        console.log("Comment added:", result);
+        setSuccessMessage("Comment created successfully!");
+        setErrorMessage("");
+        fetchComments();
+      } else {
+        const error = await response.json();
+        setErrorMessage(error.error || "Failed to create comment");
+      }
+    } catch (error) {
+      console.error("Error creating comment:", error);
+      setErrorMessage("Something went wrong. Please try again.");
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % comments.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [comments.length]);
+
+  return (
+    <div className="w-full p-8 bg-[#111827]">
+      <div className="mt-8">
+        <div className="relative w-full flex flex-col items-center mt-8">
+          <h2 className="text-white font-semibold text-2xl mb-2">Comments</h2>
+          <div className="relative w-full md:h-28 overflow-hidden rounded-lg ">
+            {comments.length > 0 ? (
+              comments.map((comment, index) => (
+                <motion.div
+                  key={comment.$id}
+                  className={`absolute w-full transition-transform duration-700 ease-in-out transform ${
+                    index === currentIndex
+                      ? "translate-x-0"
+                      : "translate-x-full"
+                  }`}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: index === currentIndex ? 1 : 0 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <li className="py-3 px-4 sm:py-4 mb-4 shadow-xl   ">
+                    <div className="flex items-center">
+                      <div className="flex-shrink-0">
+                        <img
+                          className="w-8 h-8 rounded-full"
+                          src="https://i.pinimg.com/originals/a0/e5/c5/a0e5c5602434853db7d94889eb65a106.gif" // Update with actual profile picture source if available
+                          alt={`${comment.name} image`}
+                        />
+                      </div>
+                      <div className="flex-1 min-w-0 ms-4">
+                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                          {comment.name}
+                        </p>
+                        <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                          {comment.email}
+                        </p>
+                      </div>
+                      <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        {comment.description}
+                      </div>
+                    </div>
+                  </li>
+                </motion.div>
+              ))
+            ) : (
+              <p className="text-gray-400">No comments yet.</p>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <Modal>
+        <CommentModalTrigger
+          setName={setName}
+          setEmail={setEmail}
+          setMessage={setMessage}
+        />
+        <CommentModal
+          name={name}
+          setName={setName}
+          email={email}
+          setEmail={setEmail}
+          message={message}
+          setMessage={setMessage}
+          successMessage={successMessage}
+          errorMessage={errorMessage}
+          setSuccessMessage={setSuccessMessage}
+          setErrorMessage={setErrorMessage}
+          handleSubmit={handleSubmit}
+        />
+      </Modal>
+    </div>
+  );
+};
+
+export default Commentaires;
