c4693f14b2b0b07f508fdea994d761c227ca3816
adhamito <aalmechkor2@gmail.com>
2024-10-23 10:41:00 +0100

    Fix Appwrite client endpoint setup

--- app/lib/appwrite_client.ts
+++ app/lib/appwrite_client.ts
@@ -1,12 +1,9 @@
-// /lib/appwrite_client.ts
 import { Client, Databases } from "appwrite";
 
 const client = new Client();
 
 client
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "")
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "");
 
-const databases = new Databases(client);
-
-export { databases, client };
+export const databases = new Databases(client);
