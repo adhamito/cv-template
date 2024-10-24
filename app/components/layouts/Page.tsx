cf9693625858b866e62312c1016c385c8bf1d718
adhamito <aalmechkor2@gmail.com>
2024-10-24 14:00:02 +0100

    Update Page layout component

--- app/components/layouts/Page.tsx
+++ app/components/layouts/Page.tsx
@@ -15,7 +15,7 @@ export const Page: FC<PageProps> = ({ children, variant, className }) => {
       className={cn(
         `relative bg-gray-900 z-30 overflow-hidden`,
         {
-          "md:min-w-[210mm] md:w-[210mm] sm:w-[100mm] lg:w-[210mm] w-[100mm] xl:w-[250mm] 2xl:w-[300mm] md:h-[297mm] lg:h-[297mm] sm:h-fit h-fit md:m-1 ":
+          "mb-4 md:min-w-[210mm] md:w-[210mm] sm:w-[100mm] lg:w-[210mm] w-[100mm] xl:w-[250mm] 2xl:w-[300mm] md:h-[297mm] lg:h-[297mm] xl:h-[320mm] sm:h-fit h-fit md:m-1 ":
             variant === "A4",
         },
         className
